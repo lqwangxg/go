db931a0af2a3f3eff0806fdf6f7ba041fb413a50
lqwangxg <lqwangxg@gmail.com>
2020-11-09 05:41:11 +0000

    first add files from other branch

---
+++ webapp/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+"net/http"
+"github.com/labstack/echo"
+"github.com/labstack/echo/middleware"
+"./controllers"
+)
+
+func main() {
+   e := echo.New()
+
+   e.Use(middleware.Logger())
+   e.Use(middleware.Recover())
+   e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+     AllowOrigins: []string{"*"},
+     AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.POST, echo.DELETE},
+   }))
+   e.GET("/", func(c echo.Context) error {
+     return c.String(http.StatusOK,"Hello, wolrd!, this is from server written by golang.")
+   })
+   e.POST("/price", controllers.GrabPrice) //price endpoint
+
+   //Server 
+   e.Logger.Fatal(e.Start(":8000"))
+}
+
+
